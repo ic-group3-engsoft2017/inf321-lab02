ab031293f93179b7e7166b9ae0c9875449ee510b
Diego Galhardi <diegowsu@gmail.com>
2017-10-09 21:48:26 -0300

    adicionado servico para consulta de status de entrega

---
+++ src/main/java/br/unicamp/bookstore/model/StatusEntrega.java
@@ -0,0 +1,5 @@
+package br.unicamp.bookstore.model;
+//TODO
+public class StatusEntrega {
+
+}
--- src/test/java/entrega/ConsultaEntregaStep.java
+++ src/test/java/entrega/ConsultaEntregaStep.java
@@ -4,9 +4,11 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
 
 import java.util.Map;
 
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -15,6 +17,9 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 
 import br.unicamp.bookstore.*;
 import br.unicamp.bookstore.dao.ConsultaEntregaDAO;
+import br.unicamp.bookstore.model.StatusEntrega;
+import br.unicamp.bookstore.service.BuscaEnderecoService;
+import br.unicamp.bookstore.service.BuscaStatusEntregaService;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
@@ -26,10 +31,17 @@ public class ConsultaEntregaStep {
 
 	@Mock
 	private Configuracao configuration;
+	
+	@InjectMocks
+	private BuscaStatusEntregaService buscaStatusEntregaService;
 
 	private ConsultaEntregaDAO ConsultaEntregaDAO;
 
 	private String codigorastreio;
+	
+	private StatusEntrega statusentrega;
+	
+	
 
 	private br.unicamp.bookstore.ConsultaEntrega ConsultaEntrega;
 
@@ -56,7 +68,7 @@ public class ConsultaEntregaStep {
 		String codigoRastreio = "";
 
 		wireMockServer.stubFor(get(urlMatching("/ws/" + codigoRastreio + ".*")).willReturn(aResponse().withStatus(200)
-				.withHeader("Content-Type", "text/xml").withBodyFile("resultado-pesquisa-StatusE	ntrega.xml")));
+				.withHeader("Content-Type", "text/xml").withBodyFile("resultado-pesquisa-ConsultaEntrega.xml")));
 
 	}
 
@@ -71,6 +83,7 @@ public class ConsultaEntregaStep {
 
 	@When("^eu informo o Codigo de rastreio na busca de status de entrega$")
 	public void eu_informo_codigo_rastreio() throws Throwable {
+		throwable = catchThrowable(() -> this.statusentrega = buscaStatusEntregaService.buscar(codigorastreio));
 	}
 
 	@Then("^o resultado deve ser o$")
